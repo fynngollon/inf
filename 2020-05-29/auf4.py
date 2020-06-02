dab86800e79293075c592d316338eeb08e065d8e
Fynn G <pilsygaming@gmail.com>
2020-06-02 17:42:28 +0200

    He double-clicked the magic batch!

--- 2020-05-29/auf4.py
+++ 2020-05-29/auf4.py
@@ -8,36 +8,42 @@ class Simulator(object):
         self.wurzel = self.dombaum.documentElement
         self.zustand = None
 
-        self.currentPos = self.anfangszustand()
-        #for line in eingabe:
+        self.anfangszustand()
+        for letter in eingabe:
+            self.naechsterzustand(letter)
+        if self.endzustand():
+            print("Eingabe akzeptiert.")
+        else:
+            print("Falsche Eingabe.")
 
     def anfangszustand(self):
         k_initial_liste = self.wurzel.getElementsByTagName("initial")
         if k_initial_liste != []:
             k_state = k_initial_liste[0].parentNode
-            self.zustand = k_state.getAttribute("name")
+            self.zustand = int(k_state.getAttribute("id"))
 
     def naechsterzustand(self, eingabe):
-            fromliste = baum.getElementsByTagName("from")
-            for i in fromliste:
-                if zustand == int(i.firstChild.nodeValue):
-                    readliste = i.parentNode.getElementsByTagName("read")
-                    for z in readliste:
-                        if eingabe == z.firstChild.nodeValue:
-                            transitionliste = i.parentNode.getElementsByTagName("to")
-                            return int(transitionliste[0].firstChild.nodeValue)
-            return False
+        fromliste = self.wurzel.getElementsByTagName("from")
+        for i in fromliste:
+            if self.zustand == int(i.firstChild.nodeValue):
+                readliste = i.parentNode.getElementsByTagName("read")
+                for z in readliste:
+                    if eingabe == z.firstChild.nodeValue:
+                        transitionliste = i.parentNode.getElementsByTagName("to")
+                        self.zustand = int(transitionliste[0].firstChild.nodeValue)
+
+        return None
 
     def endzustand(self):
-            k_state_liste = baum.getElementsByTagName("state")
-            for i in k_state_liste:
-                if zustand == int(i.getAttribute("id")):
-                    final = i.getElementsByTagName("final")
-                    if final != []:
-                        return True
-                    else:
-                        return False
-            return False
+        k_state_liste = self.wurzel.getElementsByTagName("state")
+        for i in k_state_liste:
+            if self.zustand == int(i.getAttribute("id")):
+                final = i.getElementsByTagName("final")
+                if final != []:
+                    return True
+                else:
+                    return False
+        return False
 
     def getZustand(self):
         return self.zustand
