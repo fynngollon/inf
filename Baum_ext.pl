171a6c04f39460a58c234e5db2d5e95c1f25447e
fynng <47257330+fynngollon@users.noreply.github.com>
2021-06-29 16:06:34 +0200

    Delete Baum_ext.pl

--- Baum_ext.pl
+++
@@ -1,4 +0,0 @@
-suche(SK, SK, ZK).
-#suche(SK, SUCHE, ZK):- knoten(SUCHE, SNUM), parent_l_r(SK, KL, KR), 
-suche(SK, SUCHE, ZK):- parent_l_r(SK, KL, KR), suche(KL, SUCHE, ZK).
-suche(SK, SUCHE, ZK):- parent_l_r(SK, KL, KR), suche(KR, SUCHE, ZK).
